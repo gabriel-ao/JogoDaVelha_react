b042f778a6f3d011368f421e01db45b6126314cf
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2020-02-15 16:24:10 -0300

    organizando divs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from "react"
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   componentDidMount() {
-    this.loadVelha()
+    this.loadVelha();
   }
 
-  loadVelha = async () => {}
+  loadVelha = async () => {};
 
   render() {
-    const renderVelha = () => (
-      <div
-        className="div_velha"
-        style={{ display: "flex", width: 600, height: 600, background: "blue" }}
-      ></div>
-    )
+    const renderVelha = () => {
+      const renderLinhas = () => {
+        const renderPosicao = () => {
+          return (
+            <div
+              className="div_coluna"
+              style={{
+                width: 200,
+
+                borderRight: "solid",
+                borderBottom: "solid"
+              }}
+            ></div>
+          );
+        };
+
+        return (
+          <div
+            className="div_linha"
+            style={{
+              display: "flex",
+              width: 600,
+              height: 200
+            }}
+          >
+            {renderPosicao()}
+            {renderPosicao()}
+            {renderPosicao()}
+          </div>
+        );
+      };
+
+      return (
+        <div className="div_velha" style={{ width: 600, height: 600 }}>
+          {renderLinhas()}
+          {renderLinhas()}
+          {renderLinhas()}
+        </div>
+      );
+    };
+
     const renderTitulo = () => (
-      <h1 style={{ textAlign: "center" }}>Jogo da velha</h1>
-    )
+      <h1 className="titulo" style={{ textAlign: "center" }}>
+        Jogo da velha
+      </h1>
+    );
 
     return (
       <div className="div_principal">
@@ -39,7 +76,7 @@ class App extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
-export default App
+export default App;
